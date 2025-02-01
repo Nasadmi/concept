c62d5814efa2d602466705d03476d8c1535fd15e
Nasadmi <samueldiego828@gmail.com>
2025-02-01 16:03:35 +0100

    Update main files

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,57 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
+import { MarkmapEntity } from './entities/markmap.entity';
+import { UserModule } from './user/user.module';
+import { DataSource } from 'typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { LoggerModule } from 'nestjs-pino';
+import { render } from 'prettyjson';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '3306'),
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_NAME || 'metalk',
+      entities: [UserEntity, MarkmapEntity],
+      synchronize: process.env.NODE_ENV === 'dev',
+    }),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_KEY || 'secret',
+      signOptions: { expiresIn: '1d' },
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        stream: {
+          write: (msg: string) => {
+            console.log(render(JSON.parse(msg)));
+          },
+        },
+      },
+    }),
+    UserModule,
+  ],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
   await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+  if (process.env.NODE_ENV === 'dev') {
+    app.useLogger(app.get(Logger));
+  }
 }
 bootstrap();
