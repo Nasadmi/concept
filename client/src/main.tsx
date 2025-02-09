fa39a28b43ac802ba967ab98f4b587cb50a31a24
Nasadmi <samueldiego828@gmail.com>
2025-02-09 22:39:51 +0100

    Add: Routes for main react files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,18 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App';
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import { StrictMode } from 'react'
+import { Home } from './components/Home';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/signin' element={<Home type="signin" />} />
+        <Route path='/login' element={<Home type="login" />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 )
