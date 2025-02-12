e6f5cc0772cdb6ed5abb2968e7e7fd075aad378e
Nasadmi <samueldiego828@gmail.com>
2025-02-12 20:13:20 +0100

    Update: add fething markmap method

--- client/src/service/fetch.service.ts
+++ client/src/service/fetch.service.ts
@@ -1,9 +1,11 @@
 import { SERVER_URL } from '../consts';
 import { UserInterface, PUserInterface } from '../types/user.interface';
+import { MkmType, PMkmType } from '../types/markmap.interface';
 
 type DataFetching = UserInterface | PUserInterface
 type Methods = 'POST' | 'GET'
-type fetchingUserParams = {url: string; data?: DataFetching, bearer?: string; method?: Methods}
+type fetchingUserParams = { url: string; data?: DataFetching, bearer?: string; method?: Methods }
+type fetchingMarkmapParams = { url: string; data?: MkmType | PMkmType; bearer?: string; method?: Methods }
 
 export const fetchingUser = async ({
   url,
@@ -12,6 +14,22 @@ export const fetchingUser = async ({
   method
 }: fetchingUserParams) => {
   return fetch(`${SERVER_URL}/${url}`, {
+    ...(data !== undefined && { body: JSON.stringify(data) }),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+      ...(bearer && { Bearer: `${bearer}` })
+    },
+    ...(method && { method }),
+  })
+}
+
+export const fetchingMarkmap = async ({
+  url,
+  data,
+  bearer,
+  method
+}: fetchingMarkmapParams) => {
+  return fetch(`${SERVER_URL}/markmap/${url}`, {
     ...(data !== undefined && { body: JSON.stringify(data) }),
     headers: {
       "Content-Type": "application/json; charset=UTF-8",
