37ef1a6681b3f974887d3213f8835dcac9d92977
Nasadmi <samueldiego828@gmail.com>
2025-03-05 12:37:54 +0100

    Change: highlight styles and codemirro width

--- client/src/components/Editor.tsx
+++ client/src/components/Editor.tsx
@@ -168,7 +168,7 @@ export const Editor = () => {
             <CodeMirror 
               value={`${config}${mkm.code === '_' ? '' : decodeBase64(mkm.code || '')}`}
               height='100vh'
-              width='350px'
+              width='750px'
               theme={light ? tokyoNightDay : tokyoNightStorm}
               extensions={[markdown({ base: markdownLanguage, codeLanguages: languages })]}
               className='maven_pro'
